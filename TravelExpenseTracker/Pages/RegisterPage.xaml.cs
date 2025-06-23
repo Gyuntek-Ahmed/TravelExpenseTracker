220df3a84f057a9708353927aa4c3c7d7a51c260
Gyuntek Ahmed <gyuntek.ahmed@sistechnology.com>
2025-06-23 10:35:11 +0300

    Add project files.

---
+++ TravelExpenseTracker/Pages/RegisterPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace TravelExpenseTracker.Pages;
+
+public partial class RegisterPage : ContentPage
+{
+	public RegisterPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ TravelExpenseTracker/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TravelExpenseTracker.ViewModels
+{
+    public partial class LoginViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _username;
+
+        [ObservableProperty]
+        private string _password;
+    }
+}
